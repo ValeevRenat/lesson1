5ac1717fce0ca6aba774ebdde729e7ee567c8810
bro <66143458+ValeevRenat@users.noreply.github.com>
2024-11-21 18:55:00 +0500

    Add files via upload

---
+++ Module_1_Variables.py
@@ -0,0 +1,6 @@
+python_course = "Python"
+number_of_tasks = 12
+number_of_hourse = 1.5
+average_time =  number_of_hourse / number_of_tasks
+average_time_min = average_time * 60
+print(python_course + " всего задач: " + str(number_of_tasks) + " затрачено часов : " + str(number_of_hourse) + " среднее время выполнения : " + str(average_time) + " или " + str(average_time_min) + " минут " )
